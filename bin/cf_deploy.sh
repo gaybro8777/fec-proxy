365ee02a4521c71136bf88d6689cc38e7252daed
Jun Li <jli@fec.gov>
2020-11-16 16:48:50 -0500

    test apply-internal-app-route

--- bin/cf_deploy.sh
+++ bin/cf_deploy.sh
@@ -11,7 +11,8 @@ org=${2}
 branch=${3}
 
 # Get the space that corresponds with the branch name
-if [[ ${branch} == "develop" ]]; then
+# if [[ ${branch} == "develop" ]]; then
+if [[ ${branch} == "feature/test-289-apply-internal-app-route-2" ]]; then
     echo "Branch is develop, deploying to dev space"
     space="dev"
 elif [[ ${branch} == release* ]]; then
