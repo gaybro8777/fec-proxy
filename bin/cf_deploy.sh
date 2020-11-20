e77293945a5d75c0b7e0165c209318c115dee139
Laura Beaufort <lbeaufort@fec.gov>
2020-11-20 14:24:36 -0500

    temporary deploy to dev

--- bin/cf_deploy.sh
+++ bin/cf_deploy.sh
@@ -11,7 +11,7 @@ org=${2}
 branch=${3}
 
 # Get the space that corresponds with the branch name
-if [[ ${branch} == "develop" ]]; then
+if [[ ${branch} == "develop" || ${branch} == "feature/eregs-545-redirects" ]]; then
     echo "Branch is develop, deploying to dev space"
     space="dev"
 elif [[ ${branch} == release* ]]; then
